71746c947f2211ff96e0c3906287509273da0eab
Jose <64102720+Jose-Rodolfo-Llave@users.noreply.github.com>
2022-05-10 23:24:23 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -23,6 +23,36 @@ public class Exercise2 {
 	public Integer getMenorNumeroSaltos(int a []) {
 
 		// TO DO
-		return -1;
+				int contador = 0;
+		int salto = 50;
+		for (int i = 0; i < a.length; i++) {
+			if (i < a.length - 1) {
+				if(a[i] == salto) {
+					if((a[i] - salto) <= salto) {
+						salto = 50 + a[i];
+						contador++;
+						i++;
+					} else {
+						return -1;
+					}
+				} else if (a[i] > salto) {
+					if((a[i] - salto) <= salto) {
+						salto = 50 + a[i-1];
+						contador++;
+						i--;
+					} else {
+						return -1;
+					}
+				}
+				
+			} else if(i == a.length - 1) {
+				if(a[i] <= salto) {
+						contador++;
+					} else {
+						return -1;
+					}
+				} 
+			}
+		return contador;
 	}
 }
